14a671a42bf025cbb0ce1f80dbb288184ca853be
joseaurelio <jatenzared@gmail.es>
2020-09-28 16:22:06 +0200

    shooter and aliens

--- scripts/index.js
+++ scripts/index.js
@@ -58,7 +58,48 @@ const addprincess = (index) => cells[index].classList.add('princess');
 const removeprincess = (index) => cells[index].classList.remove('princess');
 
 addprincess(princessPosition);
-
 console.log(princessPosition);
 
 window.addEventListener('keypress', WAD);
+
+let wafflecatPosition = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  29,
+];
+
+const addwafflecat = (index) => cells[index].classList.add('wafflecat');
+
+wafflecatPosition.forEach((index) => {
+  addwafflecat(index);
+});
+
+addwafflecat(wafflecatPosition);
+console.log(wafflecatPosition);
