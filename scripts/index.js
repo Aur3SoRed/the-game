ebd0a5f09dcdcbfcb7785b207cb5d70911878ded
joseaurelio <jatenzared@gmail.es>
2020-09-28 10:40:46 +0200

    innerText

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ const cells = [];
 for (let index = 0; index < cellCount; index = index + 1) {
   console.log(index);
   const cell = document.createElement('div');
-  // cell.innerText = index;
+  cell.innerText = index;
 
   grid.appendChild(cell);
   cells.push(cell);
